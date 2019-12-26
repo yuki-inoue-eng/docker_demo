be3bc26793157ee4bc614879540a823ba15aff00
yukiinoue <yuki.inoue.eng@gmail.com>
2019-12-26 14:12:58 +0900

    ポート指定機能を追加

--- main.go
+++ main.go
@@ -4,12 +4,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 func main() {
+	port, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatalf("Port specifiction error: %v", err)
+	}
 	http.HandleFunc("/", indexHandler)
 	fmt.Println("server start!")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
